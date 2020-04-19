f2d0c3a17f56fb65047c80e0a7268672f389f599
Uwe Riegel <uriegel@hotmail.de>
2020-04-19 09:09:35 +0200

    Another test for initializing scrollbarHeight

--- Scrollbar.vue
+++ Scrollbar.vue
@@ -38,7 +38,6 @@ export default Vue.extend({
     },
     watch: {
         parentHeight: function () {
-            console.log("ParentHeight set", this.parentHeight)
             this.refresh()
         },
         value: function (newVal) {
@@ -118,10 +117,13 @@ export default Vue.extend({
             this.$emit('input', this.position)
         },
         refresh() {
-            console.log("Refresh")
             setTimeout(() => {
                 this.scrollbarHeight = this.$refs.scrollbar.clientHeight 
-                console.log("scrollbarHeight", this.scrollbarHeight)
+                if (!this.scrollbarHeight)
+                    setTimeout(() => {     
+                        this.scrollbarHeight = this.$refs.scrollbar.clientHeight 
+                        this.setPosition(Math.min(this.range -1, this.position))
+                    }, 1000)
                 this.setPosition(Math.min(this.range -1, this.position))
             }, 200)            
         }
