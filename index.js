e26e1beec53b5a8bfbba0ff2e7d0e3e8ca451ee8
Uwe Riegel <uriegel@hotmail.de>
2020-09-16 11:21:54 +0200

    install with function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import TableView from "./TableView.vue"
 
 export default {
-    install(Vue, options) {
+    install: function(Vue, options) {
         // Let's register our component globally
         // https://vuejs.org/v2/guide/components-registration.html
         Vue.component("table-view", TableView)
